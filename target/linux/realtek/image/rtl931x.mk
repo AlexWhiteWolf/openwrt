603356e492240ef15ad241ae315bccd20e9b6d86
andrewjlamarche <andrewjlamarche@gmail.com>
2025-08-12 10:51:19 -0400

    realtek: add support for Hasivo S1300WP-8XGT-4S+

--- target/linux/realtek/image/rtl931x.mk
+++ target/linux/realtek/image/rtl931x.mk
@@ -1,1 +1,10 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
+define Device/hasivo_s1300wp_8xgt_4splus
+  SOC := rtl9313
+  DEVICE_VENDOR := Hasivo
+  DEVICE_MODEL := S1300WP-8XGT-4S+
+  IMAGE_SIZE := 7168k
+  $(Device/kernel-lzma)
+endef
+TARGET_DEVICES += hasivo_s1300wp_8xgt_4splus
